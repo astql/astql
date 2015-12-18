9265a4a0867689413a3818fd0b9da6c2d82ef1c7
Toni Bagur <toni.bagur@gmail.com>
2015-12-18 18:47:34 +0100

    pyfile

--- tests/astql.py
+++ tests/astql.py
@@ -2,33 +2,62 @@
 # -*- coding: utf-8 -*-
 import os
 import ast
+import copy
 
-class KwConstructorMixin(object):
+class BaseConstructorMixin(object):
+    def setUp(self):
+        pass
+
+class KwConstructorMixin(BaseConstructorMixin):
     def __init__(self,**kwargs):
         for k in kwargs:
             setattr(self,k,kwargs[k])
-class ArgsConstructorMixin(object):
+        self.setUp()
+class ArgsConstructorMixin(BaseConstructorMixin):
     def __init__(self,*args):
         self.args=[]
         for a in args:
             self.args.append(a)
+        self.setUp()
 
 class Query(KwConstructorMixin):
     def get_results(self):
+        self.result_dict={}
         for root,dirs,files in os.walk(self.start):
             for f in files:
                 if f.endswith('.py'):
-                    self.process_python_file(root,f)
-        return []
+                    for result in self.process_python_file(root,f):
+                        yield result
+        return
+        yield
     
-    def process_python_file(self,root,file_name):
-        file_content=open(os.path.join(root,file_name),'r').read()
-        self.pattern.feed('python_file',file_content=file_content)
+    def process_python_file(self,relative_dir,file_name):
+        file_content=open(os.path.join(relative_dir,file_name),'r').read()
+        for result in self.pattern.node_enter('python_file',self.result_dict,
+                                              file_content=file_content,
+                                              name=file_name,
+                                              relative_dir=relative_dir):
+            yield result
         tree=ast.parse(file_content)
+        
+        self.pattern.node_exit('python_file',self.result_dict,file_content=file_content)
+        return
+        yield
         
         
 class PyFile(KwConstructorMixin):
-    pass
+    def node_enter(self,pattern_type,result_dict,*args,**kwargs):
+        if pattern_type=='python_file':
+            result_dict[self.var]={'name':kwargs['name'],
+                                   'relative_dir':kwargs['relative_dir'],
+                                   'num_lines':len(kwargs['file_content'].split('\n'))
+                                  }
+            yield copy.copy(result_dict)
+        return
+        yield
+    def node_exit(self,pattern_type,result_dict,*args,**kwargs):
+        if pattern_type=='python_file':
+            del result_dict[self.var]
 
 class PyClass(KwConstructorMixin):
     pass
@@ -43,4 +72,17 @@ class PyString(KwConstructorMixin):
     pass
 
 class Stack(ArgsConstructorMixin):
-    pass
+    def setUp(self):
+        self.level=0
+    def node_enter(self,pattern_type,result_dict,*args,**kwargs):
+        yielded=False
+        if self.level<len(self.args):
+            for result in self.args[self.level].node_enter(pattern_type,result_dict,*args,**kwargs):
+                yielded=True
+                yield result
+            if yielded:
+                self.level+=1
+        return 
+        yield
+    def node_exit(self,pattern_type,result_dict,*args,**kwargs):
+        pass
--- tests/test.py
+++ tests/test.py
@@ -5,20 +5,33 @@ from mock import MagicMock as mm
 from astql import Query,PyFile,PyClass,PyMethod,And,PyString,Stack
 
 class TestQueryBasic(TestCase):
-    def test1(self):
+    def testPyFile(self):
+        pattern=PyFile(var='pyfile')
+        query=Query(start='tests/examples/example1',
+                    pattern=pattern)
+        self.assertEqual(query.start,'tests/examples/example1')
+        self.assertEqual(query.pattern,pattern)
+        results=[x for x in query.get_results()]
+        r=results[0]
+        self.assertEqual(r['pyfile']['name'],'example1.py')
+        self.assertEqual(r['pyfile']['relative_dir'],'tests/examples/example1')
+        self.assertEqual(r['pyfile']['num_lines'],5)
+        
+      
+    def testStack(self):
         pattern=Stack(PyFile(var='pyfile'),
                       PyClass(var='cl'),
                       PyMethod(var='m'),
                       And(PyString(var='str1',content='inv.agreement'),
                           PyString(var='str2',content='invoice_date')))
-        query=Query(start='tests/examples',
+        query=Query(start='tests/examples/example1',
                     pattern=pattern)
         self.assertEqual(query.start,'tests/examples/example1')
         self.assertEqual(query.pattern,pattern)
         results=[x for x in query.get_results()]
         r=results[0]
         self.assertEqual(r.pyfile.name,'example1.py')
-        self.assertEqual(r.pyfile.relative_dir,'example1')
+        self.assertEqual(r.pyfile.relative_dir,'tests/examples/example1')
         self.assertEqual(r.pyfile.num_lines,5)
         self.assertEqual(r.cl.name,'Class1')
         self.assertEqual(r.cl.line_start,1)
